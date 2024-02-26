b3d06b0379d93c53f10d688e434ec0e06eb1e0b9
Mattéo <ofb_bbx@proton.me>
2024-02-26 22:10:44 +0100

    hug + kiss command

---
+++ src/commands/Fun/Hug.ts
@@ -0,0 +1,60 @@
+import axios from "axios";
+import Command from "../../base/classes/Command";
+import CustomClient from "../../base/classes/CustomClient";
+import Category from "../../base/enums/Category";
+import {
+  ApplicationCommandOptionType,
+  ChatInputCommandInteraction,
+  PermissionsBitField,
+} from "discord.js";
+import GuildConfig from "../../base/schemas/GuildConfig";
+
+export default class Hug extends Command {
+  constructor(client: CustomClient) {
+    super(client, {
+      name: "hug",
+      description: `Show your affection by hugging someone`,
+      dev: false,
+      cooldown: 3,
+      category: Category.Fun,
+      options: [
+        {
+          name: "target",
+          description: `The target to hug`,
+          type: ApplicationCommandOptionType.User,
+          required: true,
+        },
+      ],
+      dm_permission: false,
+      default_member_permissions:
+        PermissionsBitField.Flags.UseApplicationCommands,
+      premium: false,
+    });
+  }
+  async Execute(interaction: ChatInputCommandInteraction) {
+    let apiUrl = `https://nekos.life/api/v2/img/hug`;
+    let target = interaction.options.getUser("target");
+    let guild = await GuildConfig.findOne({ id: interaction.guildId });
+    await interaction.deferReply();
+    try {
+      const response = await axios.get(apiUrl);
+      const gifUrl = response.data.url;
+
+      interaction.editReply({
+        content:
+          guild && guild.language === "fr"
+            ? `${interaction.user} a fait un calin à ${target}`
+            : `${interaction.user} hugged ${target}`,
+        embeds: [
+          {
+            image: {
+              url: gifUrl,
+            },
+          },
+        ],
+      });
+    } catch (err) {
+      return interaction.editReply(`An error occured while trying to hug.`);
+    }
+  }
+}
---
+++ src/commands/Fun/Kiss.ts
@@ -0,0 +1,57 @@
+import axios from "axios";
+import Command from "../../base/classes/Command";
+import CustomClient from "../../base/classes/CustomClient";
+import Category from "../../base/enums/Category";
+import {
+  ApplicationCommandOptionType,
+  ChatInputCommandInteraction,
+  PermissionsBitField,
+} from "discord.js";
+import GuildConfig from "../../base/schemas/GuildConfig";
+
+export default class kiss extends Command {
+  constructor(client: CustomClient) {
+    super(client, {
+      name: "kiss",
+      description: `Show your affection by kissing someone`,
+      dev: false,
+      cooldown: 3,
+      category: Category.Fun,
+      options: [
+        {
+          name: "target",
+          description: `The target to kiss`,
+          type: ApplicationCommandOptionType.User,
+          required: true,
+        },
+      ],
+      dm_permission: false,
+      default_member_permissions:
+        PermissionsBitField.Flags.UseApplicationCommands,
+      premium: false,
+    });
+  }
+  async Execute(interaction: ChatInputCommandInteraction) {
+    let apiUrl = `https://nekos.life/api/v2/img/kiss`;
+    let target = interaction.options.getUser("target");
+    await interaction.deferReply();
+    let guild = await GuildConfig.findOne({ id: interaction.guildId })
+    try {
+      const response = await axios.get(apiUrl);
+      const gifUrl = response.data.url;
+
+      interaction.editReply({
+        content: guild && guild.language === 'fr' ? `${interaction.user} a embrassé ${target}` : `${interaction.user} kissed ${target}`,
+        embeds: [
+          {
+            image: {
+              url: gifUrl,
+            },
+          },
+        ],
+      });
+    } catch (err) {
+      return interaction.editReply(`An error occured while trying to kiss.`);
+    }
+  }
+}
